81b271807a5c72631ce5c634afddc512baefc707
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-12-05 10:02:05 -0700

    caladd Caladd module completed and tested

--- caladd.py
+++ caladd.py
@@ -4,11 +4,15 @@
 # ----------------------------------------
 # Campus Event Planner: Calendar Add Functions
 # ----------------------------------------
+# Imports for the caladd which are string for punctuation verification
 import string
+# date_verify for date verification
 import date_verify
+import cal
 
+# Class definition for Event
 class Event:
-
+    # Each event object must have date, location, cap, cost_per, tags, and description text
     date = "1"
     location = ""
     cap = 0
@@ -20,7 +24,7 @@ class Event:
         self.name = name
     
     def event_rev(self):
-        return self.cost_per * self.capacity
+        return self.cost_per * self.cap
 
     def description(self):
         return self.description_txt
@@ -45,15 +49,12 @@ def dateadd(event):
                 event.date = date_temp                     
                 verify_loop = False
                 return event.date
-        # except AttributeError:
+        # except ValueError:
         #     print("AttributeError: Invalid entry please try again!!")
 
 def addlocation(event):
     while event.location == "":
         new_event_loc_raw = input("Enter the location for your event(enter cancel to exit): ")
-        # Debug Remove
-        print(type(new_event_loc_raw))
-        print(type(event))
         if new_event_loc_raw == "cancel":
             return print("\nreturning to menu.....")
         else:
@@ -62,10 +63,8 @@ def addlocation(event):
             else:
                 new_event_low = new_event_loc_raw.lower()
                 new_event1 = new_event_low[0].upper()
-                new_event2 = new_event_loc_raw[:-1]
+                new_event2 = new_event_loc_raw[1:]
                 event.location = new_event1 + new_event2 
-                # Debug Remove
-                print(event.location)
 
 def addcap(event):
     while event.cap == 0:
@@ -75,6 +74,8 @@ def addcap(event):
         else:
             try:
                 event.cap = int(new_event_cap_raw)
+                if event.cap < 0:
+                    event.cap = 0
             except ValueError:
                 print("Invalid input please try again")
 
@@ -84,28 +85,34 @@ def addprice(event):
             new_event_price_raw = input("Enter the cost per person for your event(type any letters to exit): ")
             if new_event_price_raw == "cancel":
                 return print("\nReturning to menu >>>>>>")
+            elif new_event_price raw < 0:
+                event.cost_per = 0
             else:
                 event.cost_per = float(new_event_price_raw)
         except ValueError:
             print("The number you have entered is invalid please try again")
 
 def addtags(event):
-    event.tags = "".join(char for char in input("Enter tags seperated by punctuation: ") if 
+    event.tags = ",".join(char.strip() for char in input("Enter tags seperated by punctuation: ") if 
         char not in string.punctuation)
+
+def addscript(event):
+    event.description_txt = input("Enter a description for your Event: ")
 
 def createnew(event_list):
     new_event_name = ""
     while new_event_name == "":
         new_event_raw = input("Enter the name for your event(type in cancel to exit): ").lower()
-        if new_event_raw.lower() == "cancel":
+        if new_event_raw == "cancel":
             print("\nreturning to menu.....")
             break
         elif new_event_raw in event_list:
             print("The name of the event already exists in your calendar")
         else:
-            new_event_name = new_event_raw
+            new_event_name1 = new_event_raw[0].upper()
+            new_event_name2 = new_event_raw[1:].lower()
+            new_event_name = new_event_name1 + new_event_name2
             new_event = Event(new_event_name)
-            print(new_event.name)
             dateadd(new_event)
             addlocation(new_event)
             addcap(new_event)
@@ -116,16 +123,17 @@ def createnew(event_list):
 
 # Test function call and print for debugging the caladd module 
 createnew(test)
+test_event = test[0]
 print("New Event Created!")
 print("*******\n")
-print(f"Name: {test.name}")
-print(f"Location: {test.location}")
-print(f"Date: {test.date}")
-print(f"Capacity: {test.cap}")
-print(f"Cost per person: {test.cost_per}")
-print(f"Tags: {test.tags}")
-print(f"Description: {test.description()}")
-print(f"Projected Revenue: {test.event_rev()}")
+print(f"Name: {test_event.name}")
+print(f"Location: {test_event.location}")
+print(f"Date: {test_event.date}")
+print(f"Capacity: {test_event.cap}")
+print(f"Cost per person: {test_event.cost_per}")
+print(f"Tags: {test_event.tags}")
+print(f"Description: {test_event.description()}")
+print(f"Projected Revenue: {test_event.event_rev()}")
 print("*******")
 
 
--- date_verify.py
+++ date_verify.py
@@ -15,31 +15,34 @@ def leap_check(event_date):
         return False
    
 def date_format(event_date):
-    month = int(event_date) % 100000000 // 1000000
-    day = int(event_date) % 1000000 // 10000
-    format_year = int(event_date) % 10000
-    if (month == 3 and 
-        day > 30 or month == 4 and 
-        day > 30 or month == 6 and 
-        day > 30 or month == 11 and 
-        day > 30):
+    try:
+        month = int(event_date) % 100000000 // 1000000
+        day = int(event_date) % 1000000 // 10000
+        format_year = int(event_date) % 10000
+        if (month == 3 and 
+            day > 30 or month == 4 and 
+            day > 30 or month == 6 and 
+            day > 30 or month == 11 and 
+            day > 30):
+                return False
+        elif (month == 1 and 
+              day > 31 or month == 3 and 
+              day > 31 or month == 5 and 
+              day > 31 or month == 7 and 
+              day > 31 or month == 8 and 
+              day > 31 or month == 10 and 
+              day > 31 or month == 12 and 
+              day > 31):
+                return False
+        elif leap_check(event_date) == False and day == 2 and month > 28:
             return False
-    elif (month == 1 and 
-          day > 31 or month == 3 and 
-          day > 31 or month == 5 and 
-          day > 31 or month == 7 and 
-          day > 31 or month == 8 and 
-          day > 31 or month == 10 and 
-          day > 31 or month == 12 and 
-          day > 31):
+        elif month > 0 and format_year > 1000 and day > 0 and len(event_date) == 8:
+            return True
+        else:
+            print("Unexpected Error")
             return False
-    elif leap_check(event_date) == False and day == 2 and month > 28:
-        return False
-    elif month > 0 and format_year > 1000 and day > 0 and len(event_date) == 8:
-        return True
-    else:
-        print("Unexpected Error")
-        return False
+    except ValueError:
+        print("ValueError: invalid characters found in entry please check input")
 #         if len(event_date) != 8 or event_date != "1":
 #              print("Invalid date length please try again")
 #         elif month < 1 or month > 12:
